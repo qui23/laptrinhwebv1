2484ef8633e1e00ad39450eb18bd40e261150a6b
vitdonate <vitdonate@gmail.com>
2024-01-26 00:38:24 +0700

    project

---
+++ qlthucung/Models/PaymentInformationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace qlthucung.Models;
+
+public class PaymentInformationModel
+{
+    public string OrderType { get; set; }
+    public double Amount { get; set; }
+    public string OrderDescription { get; set; }
+    public string Name { get; set; }
+}
---
+++ qlthucung/Services/IVnPayService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using qlthucung.Models;
+
+namespace qlthucung.Services;
+public interface IVnPayService
+{
+    string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+    PaymentResponseModel PaymentExecute(IQueryCollection collections);
+}
